2c4f2327db34fed41d9b4788df5ea11a5b90ccc9
David Kim <hyomithetech@gmail.com>
2021-06-01 23:57:23 -0700

    Install react navigation and create NewAlarm.js

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigation } from "@react-navigation/stack";
 
 export default function App() {
   return (
