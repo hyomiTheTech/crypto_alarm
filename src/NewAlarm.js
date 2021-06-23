c6c81d1b54817d03998026cc5430a580335091a1
David Kim <hyomithetech@gmail.com>
2021-06-22 22:45:03 -0700

    Implement select option for coin pairs

--- src/NewAlarm.js
+++ src/NewAlarm.js
@@ -1,8 +1,28 @@
 import React from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
+import RNPickerSelect from "react-native-picker-select";
 
 const NewAlarm = () => {
-  return <View>New Alarm</View>;
+  return (
+    <View>
+      Coin Pair
+      <RNPickerSelect
+        onValueChange={(value) => console.log(value)}
+        items={[
+          { label: "BTC-USD", value: "BTC-USD" },
+          { label: "ETH-USD", value: "ETH-USD" },
+          { label: "LTC-USD", value: "LTC-USD" },
+        ]}
+        placeholder={{ label: "Select Pair...", value: null }}
+      />
+      <RNPickerSelect
+        onValueChange={(value) => console.log(value)}
+        items={[{ label: "Less Than", value: "Less Than" }]}
+        placeholder={{ label: "More Than", value: "More Than" }}
+      />
+      Price
+    </View>
+  );
 };
 
 export default NewAlarm;
